a96c44c24c36d1ba857406c24da88cdb643adb7a
Daniel Meneses <daplay@gmail.com>
2013-09-24 10:52:15 -0400

    project.clj now states the right url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject read-config "0.1.0"
   :description "a function that reads an edn file"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/daplay/read-config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
